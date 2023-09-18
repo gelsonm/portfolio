486a5fe290c2f13fc62d2a864d33ed7426c28276
Moirangthem Gelson Singh <37416550+gelsonm@users.noreply.github.com>
2023-09-18 14:03:56 +0530

    Update meyawo.js

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -49,15 +49,45 @@ $(document).ready(function(){
 //     $('ul.nav').toggleClass('show');
 // });
 
-// Version 3
+// // Version 3
+// // Initially show the icon as a cross and hide the nav
+// $('#nav-toggle').addClass('is-active');
+// $('ul.nav').addClass('show');
+
+// // navbar toggle
+// $('#nav-toggle').click(function(){
+//     $(this).toggleClass('is-active');
+//     // Toggle the 'show' class on 'ul.nav' to hide/show it
+//     $('ul.nav').toggleClass('show');
+// });
+
+// Version 4
 // Initially show the icon as a cross and hide the nav
 $('#nav-toggle').addClass('is-active');
 $('ul.nav').addClass('show');
 
+// Function to toggle the navbar visibility based on screen width
+function toggleNavbar() {
+    if ($(window).width() <= 1000) {
+        // If screen width is 768 pixels or less, hide the navbar
+        $('ul.nav').removeClass('show');
+    } else {
+        // If screen width is greater than 768 pixels, show the navbar
+        $('ul.nav').addClass('show');
+    }
+}
+
+// Call the toggleNavbar function on page load
+toggleNavbar();
+
+// Call the toggleNavbar function when the window is resized
+$(window).resize(toggleNavbar);
+
 // navbar toggle
-$('#nav-toggle').click(function(){
+$('#nav-toggle').click(function() {
     $(this).toggleClass('is-active');
     // Toggle the 'show' class on 'ul.nav' to hide/show it
     $('ul.nav').toggleClass('show');
 });
+
 
